1330a3a36ae998f8373f989ebc8ac4067f557dc9
issam <issam.chaibi@gmail.com>
2015-10-23 12:28:23 +0100

    commit work 23-10-2015 filtre ajax

--- src/MyAppBundle/Controller/DefaultController.php
+++ src/MyAppBundle/Controller/DefaultController.php
@@ -214,7 +214,7 @@ class DefaultController extends Controller
         foreach ($listproducts0 as $products){ 
           $listproducts[]=$products[0];
         }
-        echo '['.count($listproducts).']';
+        //echo '['.count($listproducts).']';
         //return new Response('filtre');
         return $this->render('MyAppBundle:Default:recherche_filtre_ajax.html.twig', array('listproducts' => $listproducts));
    }
